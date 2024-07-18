03ccc72835bc135852b31268d63ad0afa1f8b4c8
Shruti Thakur <shrutithakur9508@gmail.com>
2024-07-18 15:01:04 +0530

    Update database.js

--- config/database.js
+++ config/database.js
@@ -2,10 +2,10 @@ const { createPool } = require('mysql');
 
 const pool = createPool({
     port: process.env.DB_PORT || 3306,
-    host: process.env.DB_HOST || 'bxp1iazaditltxzoz097-mysql.services.clever-cloud.com',
-    user: process.env.DB_USER || 'uz4t6jvvhry3wmwi',
-    password: process.env.DB_PASS || 'Wa7UBbIXEsiqZpCoKssw',
-    database: process.env.MYSQL_DB || 'bxp1iazaditltxzoz097',
+    host: process.env.DB_HOST || 'bly92g6wlvtvqdkvwkt7-mysql.services.clever-cloud.com',
+    user: process.env.DB_USER || 'ufwycdipmnhuyf9d',
+    password: process.env.DB_PASS || 'bly92g6wlvtvqdkvwkt7',
+    database: process.env.MYSQL_DB || 'bly92g6wlvtvqdkvwkt7',
     connectionLimit: 10 // This sets the maximum number of connections in the pool
 });
 
